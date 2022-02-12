6d4b08d11cab669b36e0cbb3bc8872a3897ea401
luis curo <lcuro@continental.edu.pe>
2022-02-12 03:40:49 +0000

    update clases 05

---
+++ app-front.com/public_html/app/productos/productos-controller.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+$('#listProductos').DataTable( {
+        "ajax":{
+            type: 'get',
+            url: "http://apis.com/api/v1/productos",
+            dataSrc: 'data',
+            cache: true
+            },
+        columns: [
+        	{
+        		"targets": 0,
+                "render": function ( data, type, row ) {
+                    return row.created_at;
+                }                
+            },
+            { data: 'codigo' },
+            { data: 'nombre' },
+            {
+        		"targets": 3,
+                "render": function ( data, type, row ) {
+                	
+
+                	if(row.categoria)
+                	{
+
+                		return row.categoria.nombre;
+                	}
+                	else
+                	{
+                		return "";
+                	}
+                    
+                }                
+            },
+            { data: 'precio' },
+            {
+        		"targets": 5,
+                "render": function ( data, type, row ) {
+                	
+                	return "<a href='#'>Editar</a> | <a href='#'>Eliminar</a>"
+                    
+                }                
+            },
+        ]
+
+    });
+
+
+
+});
